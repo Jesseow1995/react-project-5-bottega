3a94e660a3a997f20c846b8fff65d931b5afff63
Jesse Williamson <jesseowilliamson@gmail.com>
2020-08-11 19:45:33 -0600

    setup logo

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="logo_img" src="/assets/images/ds_circle_logo.png" />
+            </div>
+        )
+    }
+}
