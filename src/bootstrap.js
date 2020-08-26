f9795e66de91e791f5ea3296bf9839382c8173e2
Jesse Williamson <jesseowilliamson@gmail.com>
2020-08-26 10:35:53 -0600

    added api request for recent posts

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,7 +6,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/home";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import thunk from 'redux-thunk'
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import "./style/main.scss";
 import Results from "./components/results";
